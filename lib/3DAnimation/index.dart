31179898663a5914f2268c963108420ebab9404f
xrole <xrolediamond@gmail.com>
2021-09-09 23:06:07 +0100

    few custom paints

--- lib/3DAnimation/index.dart
+++ lib/3DAnimation/index.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:tutorial_apps/3DAnimation/sphere_ball.dart';
+import 'package:tutorial_apps/practice3D/index.dart';
 
 class AnimationHome extends StatefulWidget {
-  const AnimationHome({ Key? key }) : super(key: key);
+  const AnimationHome({Key? key}) : super(key: key);
 
   @override
   _AnimationHomeState createState() => _AnimationHomeState();
@@ -12,14 +13,21 @@ class _AnimationHomeState extends State<AnimationHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('3D Animation Example'),),
+      appBar: AppBar(
+        title: Text('3D Animation Example'),
+      ),
       body: Container(
-        padding: EdgeInsets.all(8),
-        child: Column(
-          children: [
-            const SphereBall(),
-          ],
-        ))
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              const SphereBall(),
+            ],
+          )),
+      floatingActionButton: InkWell(
+          onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => PracticePage()));
+          },
+          child: Text('Xrole')),
     );
   }
 }
